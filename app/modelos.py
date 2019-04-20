3ebcd8fcd483d86f178dbb86bc3f5e1d1fee829e
Marcelo P R Nunes <marcelo@casa.local>
2019-04-19 22:00:35 -0300

    modelos do banco

--- app/modelos.py
+++ app/modelos.py
@@ -1,4 +1,5 @@
 from peewee import *
+import datetime
 
 db = SqliteDatabase('db/nbackup.db')
 
@@ -8,18 +9,23 @@ class BaseModel(Model):
 
 class Backup(BaseModel):
 
-
+    nome = CharField()
     tipo = CharField()
     fonte = CharField()
     path_origem = CharField()
     path_destino = CharField()
     periodo = CharField()
     dia_semana = CharField()
-    hora_execucao = CharField()
+    #hora_execucao = CharField()
+    hora_execucao = DateTimeField()
     sc_pre_execucao = CharField()
     sc_pos_execucao = CharField()
     sc_backup = CharField()
     backup_auto = CharField()
+
+    def hora(self):
+        h = datetime.datetime.strptime(hora_execucao,'%H:%M')
+        return h
 
     def _format_hora(self, hora):
         h = datetime.datetime.strptime(hora,'%H:%M')
--- app/teste.py
+++ app/teste.py
@@ -37,31 +37,43 @@ except zipfile.LargeZipFile:
 
 '''
 
-db = peewee.SqliteDatabase('nbackup.db')
-db.connect()
+#db = peewee.SqliteDatabase('db/nbackup.db')
 #db.create_tables([modelos.Backup, modelos.Arquivo])
 
+'''
 backup = modelos.Backup()
+backup.nome = 'backup_1'
 backup.tipo = 'arquivo'
 backup.fonte = 'sql.dump'
-backup._path_origem = '/home/marcelo/fonte'
+backup.path_origem = '/home/marcelo/fonte'
 backup.path_destino = '/home/marcelo/backup'
 backup.periodo = 'diario'
 backup.dia_semana = ''
-backup.hora_execucao = '15:00'
+hora = datetime.time(hour=15, minute=30)
+backup.hora_execucao = hora
 backup.sc_pre_execucao = '/home/marcelo/script/script.sh'
 backup.sc_pos_execucao = '/home/marcelo/script/script.sh'
 backup.sc_backup = '/home/marcelo/script/backup.sh'
 backup.backup_auto = 'Nao'
-backup.create()
+backup.save()
 
-'''
 arquivo = modelos.Arquivo()
-arquivo.name = 'backup_1.zip'
+arquivo.nome = 'backup_1.zip'
 arquivo.path = '/home/marcelo/backup'
 arquivo.hash_verificacao = '123456d8f5g6e9d3s2d4d5f'
 arquivo.data_criacao = datetime.datetime.now()
 arquivo.backup = backup
-arquivo.create()
+arquivo.save()
 '''
-db.close()
+
+bks = modelos.Backup.select()
+for b in bks:
+    print('nome: {}'.format(b.nome))
+    print('hora_execucao: {}'.format(b.hora_execucao))
+    #print('type hora: {}'.format(type(b.hora())))
+
+a1 = modelos.Arquivo().select()
+
+for arquivo in a1:
+    print('nome backup: {}'.format(arquivo.backup.nome))
+    print('path: {}'.format(arquivo.path))
